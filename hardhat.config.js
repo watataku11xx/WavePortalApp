27550879b2dcd98d0b2bf54a8b911f2986945146
watataku11xx <watataku131122@outlook.jp>
2023-02-22 15:50:36 +0900

    [add] WavePortalコントラクトの編集
    WavePortal.sol, run.jsを編集し、コントラクトのテストを完了

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
+// hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/WCG8eobymJ-6k-YbfGtTpAE_swDM7U23",
+      accounts: ["b1c07d74838125147abff2dea5516da965d4d18cb05c40432fb16a9b28c2a37d"],
+    },
+  },
 };
